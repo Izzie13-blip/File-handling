3e46c10f1149ee301aaf93acbccb747a1e4b55e0
Izzie13-blip <marketbyamie@gmail.com>
2025-04-17 20:08:03 +0100

    Add files via upload

--- min.py
+++ min.py
@@ -2,3 +2,13 @@ f = open('Codingal.txt','r')
 print(f)
 print(f.read())
 f.close()
+
+f1 = open('Codingal.txt','w')
+w1 = f1.write('Hello World')
+#print(w1)
+f1.close()
+
+f2 = open('Codingal.txt','a')
+w2 =f2.write('Its a new day')
+#print(w1)
+f2.close()
